a2debd2f951b997659777bff969f944ad370939d
Sheng Jiang (DSI) <joji@microsoft.com>
2017-02-23 20:52:44 +0800

    write the endsWith polyfill in ES6

--- text2jsvar.js
+++ text2jsvar.js
@@ -21,15 +21,15 @@ module.exports = {
 };
 
 if (!String.prototype.endsWith) {
-  String.prototype.endsWith = function (searchString, position) {
-    var subjectString = this.toString();
-    if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
-      position = subjectString.length;
-    }
-    position -= searchString.length;
-    var lastIndex = subjectString.indexOf(searchString, position);
-    return lastIndex !== -1 && lastIndex === position;
-  };
+    String.prototype.endsWith = (searchString, position) => {
+        let subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        let lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
 }
 
 function convertEscapeChars(source) {
